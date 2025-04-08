22b97dc0e650a20f25135c4eac0e1ca5f13d1cce
Sheikh Muhammad Antor <sheikhmuhammadantor@gmail.com>
2025-04-08 13:43:49 +0600

    Deploy 1 - Comment out unused router import and related code in Client component

--- src/app/(messages)/messages/client.tsx
+++ src/app/(messages)/messages/client.tsx
@@ -1,16 +1,16 @@
 "use client";
 import React from 'react';
-import { useRouter } from "next/navigation";
+// import { useRouter } from "next/navigation";
 
 const Client = () => {
 
-    const router = useRouter();
+    // const router = useRouter();
 
   return (
     <div className='border rounded-lg p-3 border-sky-500 my-3'>
       Client
       <p>
-        {
+        { "Hi, Error"
             // router.push("/")
             // console.log(router.push)
         }
